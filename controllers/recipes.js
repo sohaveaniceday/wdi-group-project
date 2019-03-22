9ccd93e1ab7a2f8252c0eda8dd785d15364f2875
Gabriella Rose <gabriella.sophie.rose@gmail.com>
2019-03-22 12:26:51 +0000

    models, routes, controllers created

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,1 +1,53 @@
-//So many recipes not enough time. 
+//So many recipes not enough time.
+const Recipe = require('../models/recipe')
+
+function indexRoute(req, res) {
+  return Recipe
+    .find(req.query)
+    .then(recipe => res.status(200).json(recipe))
+    .catch(err => res.json(err))
+}
+
+function createRoute(req,res) {
+  return Recipe
+    .create(req.body)
+    .then(recipe => res.status(201).json(recipe))
+    .catch(err => res.json(err))
+}
+
+function showRoute(req, res) {
+  return Recipe
+    .findById(req.params.id)
+    .then(recipe=> {
+      if (!recipe) return res.status(404).json({ message: 'Not Found'})
+      res.status(200).json(recipe)
+    })
+    .catch(err => res.json(err))
+}
+
+function editRoute(req,res) {
+  return Recipe
+    .findById(req.params.id)
+    .then(recipe => {
+      if (!recipe) return res.status(404).json({ message: 'Not Found'})
+      Object.assign(recipe, req.body)
+      return recipe.save()
+    })
+    .then(recipe => res.status(200).json(recipe))
+    .catch(err => res.json(err))
+}
+
+function deleteRoute(req, res) {
+  return Recipe
+    .findByIdAndRemove(req.params.id)
+    .then(() => res.status(202).end('OK'))
+    .catch(err => res.status(404).json(err))
+}
+
+module.exports = {
+  index: indexRoute,
+  create: createRoute,
+  show: showRoute,
+  edit: editRoute,
+  delete: deleteRoute
+}
--- models/user.js
+++ models/user.js
@@ -1,1 +1,70 @@
 //Munch is awesome.
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true, trim: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String },
+  categories: [ {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Category',
+    required: true
+  } ],
+  image: { type: String },
+  location: { type: String },
+  bio: { type: String }
+})
+
+userSchema.plugin(require('mongoose-unique-validator'))
+
+userSchema.set('toJSON', {
+  transform(doc, json) {
+    delete json.password
+    return json
+  }
+})
+
+userSchema.virtual('reviews', {
+  ref: 'Reviews',
+  localField: '_id',
+  foreignField: 'user'
+})
+
+userSchema.virtual('recipes', {
+  ref: 'Recipes',
+  localField: '_id',
+  foreignField: 'user'
+})
+
+userSchema.methods.validatePassword = function validatePassword(password) {
+  return bcrypt.compareSync(password, this.password)
+}
+
+userSchema
+  .virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation) {
+    this._passwordConfirmation = passwordConfirmation
+  })
+
+userSchema
+  .pre('validate', function checkPassword(next) {
+    if (this.isModified('password') && this._passwordConfirmation !== this.password) {
+      this.invalidate('Password Confirmation', 'does not match')
+    }
+    next()
+  })
+
+userSchema
+  .pre('save', function hashPassword(next) {
+    if (this.isModified('password')) {
+      this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8))
+    }
+    next()
+  })
+
+module.exports = mongoose.model('User', userSchema)
+
+// review and recipes as virtuals
+
+// add shopping list as an mvp+
