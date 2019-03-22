d708432c75f4f708f6d4b47e684ff54e88f30ad9
Pascual <pascual.vila.segura@hotmail.com>
2019-03-22 10:42:45 +0000

    environmetnt starting

--- config/environment.js
+++ config/environment.js
@@ -1,1 +1,6 @@
 //Munch is organic
+const port = process.env.PORT || 4000
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/munch'
+const secret = process.env.SECRET || 'ssh it\'s a secret'
+
+module.exports = { port, dbURI, secret }
--- config/routes.js
+++ config/routes.js
@@ -1,1 +1,6 @@
-//Munch has dinner. 
+//Munch has dinner.
+const router = require('express').Router()
+
+
+
+module.exports = router
