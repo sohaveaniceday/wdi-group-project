017374307128c65bf986d3593cb4c0287e22e546
Semmi <semmi.semmiw@gmail.com>
2019-03-23 14:50:18 +0000

    login-register-home final update

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return(
-    <section className="hero is-warning is-fullheight-with-navbar">
+    <section className="hero is-success is-fullheight-with-navbar">
       <div className="hero-body">
         <div className="container">
           <p className="title">Munch</p>
           <p className="subtitle">Munch App</p>
+          <Link className="button is-info" to={'/register'}>Register</Link>
+          <Link className="button is-info" to={'/login'}>Login</Link>
         </div>
       </div>
     </section>
