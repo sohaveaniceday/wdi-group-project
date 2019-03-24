17a9b9f60b28627c6d5e630c2877e11b4bdd8140
Richard Turner <rclt201@gmail.com>
2019-03-24 07:02:25 +0000

    fixed seeds

--- db/seeds1.js
+++ db/seeds1.js
@@ -8,106 +8,207 @@ mongoose.connect(dbURI, { useNewUrlParser: true}, (err, db) => {
   db.dropDatabase()
 
   Category.create([
-    {	'name': 'Algerian'	},
-    {	'name': 'Allergy-Free'	},
-    {	'name': 'American'	},
-    {	'name': 'BBQ Food'	},
-    {	'name': 'Belgian'	},
-    {	'name': 'Brazilian'	},
-    {	'name': 'Bread & Doughs'	},
-    {	'name': 'Breakfast'	},
-    {	'name': 'British'	},
-    {	'name': 'Burgers'	},
-    {	'name': 'Cajun'	},
-    {	'name': 'Cakes'	},
-    {	'name': 'Canadian'	},
-    {	'name': 'Caribbean'	},
-    {	'name': 'Casserole'	},
-    {	'name': 'Cheese'	},
-    {	'name': 'Chinese'	},
-    {	'name': 'Cocktails'	},
-    {	'name': 'Cookies'	},
-    {	'name': 'Cuban'	},
-    {	'name': 'Curry'	},
-    {	'name': 'Diabetic'	},
-    {	'name': 'Dinner Party'	},
-    {	'name': 'Dinner'	},
-    {	'name': 'Drinks'	},
-    {	'name': 'Egyptian'	},
-    {	'name': 'Energy Boosting'	},
-    {	'name': 'Ethiopian'	},
-    {	'name': 'Fast Food'	},
-    {	'name': 'Fine Dining'	},
-    {	'name': 'French'	},
-    {	'name': 'German'	},
-    {	'name': 'Gluten-Free'	},
-    {	'name': 'Greek'	},
-    {	'name': 'Hawaiian'	},
-    {	'name': 'Healthy'	},
-    {	'name': 'Hearty'	},
-    {	'name': 'High-Protein'	},
-    {	'name': 'Hipster'	},
-    {	'name': 'Indian'	},
-    {	'name': 'Irish'	},
-    {	'name': 'Italian'	},
-    {	'name': 'Japanese'	},
-    {	'name': 'Kenyan'	},
-    {	'name': 'Kitsch'	},
-    {	'name': 'Korean'	},
-    {	'name': 'Lacto-Ovo Vegetarians'	},
-    {	'name': 'Lacto-Vegetarian'	},
-    {	'name': 'Lactose-Free'	},
-    {	'name': 'Latvian'	},
-    {	'name': 'Lebanese'	},
-    {	'name': 'Libyan'	},
-    {	'name': 'Low-Carb'	},
-    {	'name': 'Lunch'	},
-    {	'name': 'Mediterranean'	},
-    {	'name': 'Mexican'	},
-    {	'name': 'Moroccan'	},
-    {	'name': 'Muffins'	},
-    {	'name': 'Nigerian'	},
-    {	'name': 'No-Sodium'	},
-    {	'name': 'Non-Spicy'	},
-    {	'name': 'Nut-Free'	},
-    {	'name': 'Occasions'	},
-    {	'name': 'One Pot'	},
-    {	'name': 'Ovo-Vegetarian'	},
-    {	'name': 'Paleo'	},
-    {	'name': 'Pancakes'	},
-    {	'name': 'Pasta & Risotto'	},
-    {	'name': 'Pasta Bakes'	},
-    {	'name': 'Peruvian'	},
-    {	'name': 'Pescetarians'	},
-    {	'name': 'Pizza'	},
-    {	'name': 'Polish'	},
-    {	'name': 'Portuguese'	},
-    {	'name': 'Puddings & Desserts'	},
-    {	'name': 'Quick & Easy'	},
-    {	'name': 'Roast'	},
-    {	'name': 'Romantic'	},
-    {	'name': 'Russian'	},
-    {	'name': 'Salad'	},
-    {	'name': 'Salvadorian'	},
-    {	'name': 'Sandwiches & Wraps'	},
-    {	'name': 'Sauces & Condiments'	},
-    {	'name': 'Scottish'	},
-    {	'name': 'Sharing'	},
-    {	'name': 'Snacks'	},
-    {	'name': 'Soup'	},
-    {	'name': 'Spanish'	},
-    {	'name': 'Spicy'	},
-    {	'name': 'Steak'	},
-    {	'name': 'Stew'	},
-    {	'name': 'Swedish'	},
-    {	'name': 'Tapas'	},
-    {	'name': 'Thai'	},
-    {	'name': 'Tibetan'	},
-    {	'name': 'Turkish'	},
-    {	'name': 'Vegan'	},
-    {	'name': 'Vegetarian'	},
-    {	'name': 'Vietnamese'	},
-    {	'name': 'Welsh'	}
+    {
+      'name': 'Vegetarian'
+    }, {
+      'name': 'Chinese'
+    }, {
+      'name': 'American'
+    }, {
+      'name': 'British'
+    }, {
+      'name': 'Caribbean'
+    }, {
+      'name': 'French'
+    }, {
+      'name': 'Greek'
+    }, {
+      'name': 'Indian'
+    }, {
+      'name': 'Italian'
+    }, {
+      'name': 'Japanese'
+    }, {
+      'name': 'Mediterranean'
+    }, {
+      'name': 'Mexican'
+    }, {
+      'name': 'Moroccan'
+    }, {
+      'name': 'Spanish'
+    }, {
+      'name': 'Thai'
+    }, {
+      'name': 'Turkish'
+    }, {
+      'name': 'Vietnamese'
+    }, {
+      'name': 'Gluten-Free'
+    }, {
+      'name': 'Vegan'
+    }, {
+      'name': 'Cajun'
+    }, {
+      'name': 'Lebanese'
+    }, {
+      'name': 'Ethiopian'
+    }, {
+      'name': 'German'
+    }, {
+      'name': 'Korean'
+    }, {
+      'name': 'Swedish'
+    }, {
+      'name': 'Latvian'
+    }, {
+      'name': 'Scottish'
+    }, {
+      'name': 'Canadian'
+    }, {
+      'name': 'Russian'
+    }, {
+      'name': 'Polish'
+    }, {
+      'name': 'Brazilian'
+    }, {
+      'name': 'Hawaiian'
+    }, {
+      'name': 'Peruvian'
+    }, {
+      'name': 'Salvadorian'
+    }, {
+      'name': 'Cuban'
+    }, {
+      'name': 'Tibetan'
+    }, {
+      'name': 'Egyptian'
+    }, {
+      'name': 'Belgian'
+    }, {
+      'name': 'Irish'
+    }, {
+      'name': 'Welsh'
+    }, {
+      'name': 'Portuguese'
+    }, {
+      'name': 'Kenyan'
+    }, {
+      'name': 'Algerian'
+    }, {
+      'name': 'Nigerian'
+    }, {
+      'name': 'Libyan'
+    }, {
+      'name': 'Ovo-Vegetarian'
+    }, {
+      'name': 'Lacto-Vegetarian'
+    }, {
+      'name': 'Lacto-Ovo Vegetarians'
+    }, {
+      'name': 'Pescetarians'
+    }, {
+      'name': 'Low-Carb'
+    }, {
+      'name': 'High-Protein'
+    }, {
+      'name': 'No-Sodium'
+    }, {
+      'name': 'Lactose-Free'
+    }, {
+      'name': 'Paleo'
+    }, {
+      'name': 'Diabetic'
+    }, {
+      'name': 'Nut-Free'
+    }, {
+      'name': 'Healthy'
+    }, {
+      'name': 'Pasta & Risotto'
+    }, {
+      'name': 'Salad'
+    }, {
+      'name': 'Bread & Doughs'
+    }, {
+      'name': 'Curry'
+    }, {
+      'name': 'Soup'
+    }, {
+      'name': 'Roast'
+    }, {
+      'name': 'Stew'
+    }, {
+      'name': 'BBQ Food'
+    }, {
+      'name': 'Pizza'
+    }, {
+      'name': 'Sandwiches & Wraps'
+    }, {
+      'name': 'Cakes'
+    }, {
+      'name': 'Sauces & Condiments'
+    }, {
+      'name': 'Puddings & Desserts'
+    }, {
+      'name': 'Drinks'
+    }, {
+      'name': 'Cookies'
+    }, {
+      'name': 'Muffins'
+    }, {
+      'name': 'Pasta Bakes'
+    }, {
+      'name': 'Burgers'
+    }, {
+      'name': 'Breakfast'
+    }, {
+      'name': 'Lunch'
+    }, {
+      'name': 'Dinner'
+    }, {
+      'name': 'Pancakes'
+    }, {
+      'name': 'One Pot'
+    }, {
+      'name': 'Tapas'
+    }, {
+      'name': 'Sharing'
+    }, {
+      'name': 'Snacks'
+    }, {
+      'name': 'Spicy'
+    }, {
+      'name': 'Non-Spicy'
+    }, {
+      'name': 'Hearty'
+    }, {
+      'name': 'Allergy-Free'
+    }, {
+      'name': 'Dinner Party'
+    }, {
+      'name': 'Fast Food'
+    }, {
+      'name': 'Fine Dining'
+    }, {
+      'name': 'Quick & Easy'
+    }, {
+      'name': 'Occasions'
+    }, {
+      'name': 'Romantic'
+    }, {
+      'name': 'Hipster'
+    }, {
+      'name': 'Cheese'
+    }, {
+      'name': 'Energy Boosting'
+    }, {
+      'name': 'Alcohol'
+    }, {
+      'name': 'Alcohol-Free'
+    }, {
+      'name': 'Cocktails'
+    }, {
+      'name': 'Kitsch'
+    }
   ])
     .then(categories => console.log(`${categories.length} categories created`))
     .catch(err => console.log(err))
