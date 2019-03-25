6de52a20a5de6b0ac4b5a356a1d4caea5f70897c
Pascual <pascual.vila.segura@hotmail.com>
2019-03-25 15:06:28 +0000

    rounded inputs

--- src/components/reviews/reviewForm.js
+++ src/components/reviews/reviewForm.js
@@ -10,7 +10,7 @@ const ReviewForm = ({ handleChange, handleSubmit, handleSelect, data, errors, ca
           <label className="label">Restaurant Name (required)</label>
           <div className="control">
             <input
-              className={`input ${errors.restaurantName ? 'is-danger': ''}`}
+              className={`input is-rounded${errors.restaurantName ? 'is-danger': ''}`}
               name="restaurantName"
               placeholder="Restaurant Name"
               onChange={handleChange}
@@ -20,7 +20,7 @@ const ReviewForm = ({ handleChange, handleSubmit, handleSelect, data, errors, ca
           {errors.restaurantName && <small className="help is-danger">{errors.restaurantName}</small>}
         </div>
         <label className="label">Rating</label>
-        <div className="select">
+        <div className="select is-rounded">
           <select onChange={handleChange} name="rating">
             <option>Select Rating</option>
             <option value="1">1 Star</option>
@@ -34,7 +34,7 @@ const ReviewForm = ({ handleChange, handleSubmit, handleSelect, data, errors, ca
           <label className="label">Review</label>
           <div className="control">
             <input
-              className={`input ${errors.reviewText ? 'is-danger': ''}`}
+              className={`input is-rounded${errors.reviewText ? 'is-danger': ''}`}
               name="reviewText"
               placeholder="Review"
               onChange={handleChange}
@@ -47,7 +47,7 @@ const ReviewForm = ({ handleChange, handleSubmit, handleSelect, data, errors, ca
           <label className="label">Image</label>
           <div className="control">
             <textarea
-              className={`input ${errors.image ? 'is-danger': ''}`}
+              className={`input is-rounded ${errors.image ? 'is-danger': ''}`}
               placeholder="Image"
               name="image"
               onChange={handleChange}
@@ -68,7 +68,7 @@ const ReviewForm = ({ handleChange, handleSubmit, handleSelect, data, errors, ca
             />
           </div>
         </div>
-        <button className="button is-info">Submit</button>
+        <button className="button is-info is-rounded">Submit</button>
       </form>
     </div>
   )
