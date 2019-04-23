a97e8b31a0dc9e00c4c1b644545a876f43c5869a
Richard Turner <rclt201@gmail.com>
2019-04-23 19:29:16 +0100

    test

--- README.md
+++
@@ -1,6 +0,0 @@
-# wdi-group-project
-Group project Repo
-
-Link: https://munch-ga.herokuapp.com
-
-Test
--- models/category.js
+++
@@ -1,8 +0,0 @@
-//Munch is awesome.
-const mongoose = require('mongoose')
-
-const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true }
-})
-
-module.exports = mongoose.model('Category', categorySchema)
