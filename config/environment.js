7f0780bc7de17b0b8503e49758c6bb243b1ab024
Richard Turner <rclt201@gmail.com>
2019-05-22 11:36:22 +0100

    refactoring

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 //Munch is organic
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/munch'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/munch'
 const secret = process.env.SECRET || 'ssh it\'s a secret'
 
 module.exports = { port, dbURI, secret }
